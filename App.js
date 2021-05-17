34206196f111d7c3f27343e93114f86096f9d06a
AdityaSid <adityasid786@gmail.com>
2021-05-17 08:28:41 +0530

    learn about onPress props in Text Element

--- App.js
+++ App.js
@@ -3,11 +3,15 @@ import React from 'react';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
+
+  const handlePress = () => { console.log("Text Pressed") }
+
   return (
     <SafeAreaView style={styles.container}>
       <Text
         style={styles.fontColor}
         numberOfLines={1}
+        onPress={handlePress}
       >
         Hello React Native, A really really long text. I wanna make this even longer and see what happens!
         </Text>
@@ -20,6 +24,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'dodgerblue',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   fontColor: {
     color: 'white'
