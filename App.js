172e766205e80396ddd40ae1685c0063e37816b7
AdityaSid <adityasid786@gmail.com>
2021-05-18 18:09:11 +0530

    flex

--- App.js
+++ App.js
@@ -1,38 +1,39 @@
-// import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import {
-  StyleSheet,
-  Text,
   View,
-  SafeAreaView,
-  Platform,
-  Dimensions,
-  StatusBar
+  StyleSheet
 } from 'react-native';
 
-import {
-  useDimensions,
-  useDeviceOrientation
-} from '@react-native-community/hooks'
 
 export default function App() {
 
-  const { landscape } = useDeviceOrientation();
 
   return (
-    <SafeAreaView style={[styles.container, containerStyle]}>
+    <View
+      style={{
+        backgroundColor: "white",
+        flex: 1,
+      }}
+    >
       <View
         style={{
-          backgroundColor: 'gray',
-          width: '50%',
-          height: landscape ? '100%' : '30%',
-
+          backgroundColor: 'dodgerblue',
+          flex: 2
         }}
-      >
-        <Text style={styles.fontColor}>React Native</Text>
-      </View>
-      {/* <StatusBar /> */}
-    </SafeAreaView>
+      />
+      <View
+        style={{
+          backgroundColor: 'gold',
+          flex: 1
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: 'tomato',
+          flex: 1
+        }}
+      />
+    </View>
   );
 }
 
@@ -42,7 +43,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    // paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
     // alignItems: 'center',
     // justifyContent: 'center'
   },
