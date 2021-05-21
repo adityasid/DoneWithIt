dbbcd8348f4c5d7d8e9215692bc26063c18b35cf
AdityaSid <adityasid786@gmail.com>
2021-05-21 17:27:12 +0530

    start from 0, added logo

--- App.js
+++ App.js
@@ -1,77 +1,16 @@
 import React from 'react';
-import {
-  View,
-  StyleSheet,
-  SafeAreaView
-} from 'react-native';
-
+import { SafeAreaView, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
 
 export default function App() {
-
-
   return (
-    <View
-      style={{
-        backgroundColor: "white",
-        flex: 1,
-        flexDirection: 'row',
-        // justifyContent: 'space-between',
-        alignItems: 'stretch',
-        alignContent: 'center',
-      }}
-    >
-      <View
-        style={{
-          backgroundColor: 'dodgerblue',
-          width: 100,
-          // height: 300,
-          flexBasis: 100,
-          // flexGrow: 1,
-          // flex: -1,  // flexShrink
-          flexShrink: 1
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: 'gold',
-          width: 100,
-          height: 200,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: 'tomato',
-          width: 100,
-          height: 400,
-          top: 40,
-          left: 20,
-          position: 'absolute',
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: 'gray',
-          width: 100,
-          height: 700,
-        }}
-      />
-
-    </View>
-
+    <SafeAreaView>
+      <View>
+        <Text>
+          Hello React Native
+        </Text>
+      </View>
+      {/* <StatusBar style='auto' /> */}
+    </SafeAreaView>
   );
 }
-
-const containerStyle = { backgroundColor: "lightblue" }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-    // paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-    // alignItems: 'center',
-    // justifyContent: 'center'
-  },
-  fontColor: {
-    color: 'white'
-  },
-});
